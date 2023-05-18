3a7715d7785f7f6500c8b1a4b579724992a53426
JohannesSloth <joha218s@stud.kea.dk>
2023-05-18 16:13:31 +0200

    login and logout endpoints added to userRouter.js in server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./routers/userRouter.js"
-import db from "./database/connection.js"
+import dotenv from "dotenv";
+import session from "express-session";
+
+dotenv.config();
 
 const app = express();
 
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false },
+    })
+  );
 app.use(express.json());
 app.use(
     cors({
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -11,8 +11,28 @@ router.post(
   "/api/signup",
   apiLimiter,
   [
-    body("username").notEmpty().withMessage("Username is required"),
-    body("email").notEmpty().withMessage("Invalid Email Adress"),
+    body("username")
+      .notEmpty()
+      .withMessage("Username is required")
+      .custom(async (value) => {
+        const user = await db.users.findOne({ username: value });
+        if (user) {
+          throw new Error("Username already in use");
+        }
+        return true;
+      }),
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email address"),
+    /*.custom(async (value) => {
+        const user = await db.users.findOne({ email: value });
+        if (user) {
+          throw new Error('Email already in use');
+        }
+        return true;
+      }),*/
     body("password")
       .isLength({ min: 6 })
       .withMessage("Password must be at least 6 characters long"),
@@ -29,18 +49,49 @@ router.post(
       console.error(err);
       return res.status(500).json({ error: "Error hashing password" });
     });
-    
+
     const user = { username, email, password: hashedPassword };
-    
+
     try {
-        await db.users.insertOne(user);
-        await sendSignedUpMail(username, email);
-        res.status(200).json({ message: "User created successfully" });
-      } catch (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Error creating user" });
-      }
+      await db.users.insertOne(user);
+      await sendSignedUpMail(username, email);
+      res.status(200).json({ message: "User created successfully" });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error creating user" });
+    }
   }
 );
+
+router.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await db.users.findOne({ username: username });
+  if (!user) {
+    return res.status(400).json({ error: "Invalid username or password" });
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res.status(400).json({ error: "Invalid username or password" });
+  }
+
+  req.session.userId = user._id;
+
+  res.status(200).json({ message: "Logged in successfully" });
+});
+
+router.post("/api/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Could not log out, please try again" });
+    }
+
+    res.clearCookie("connect.sid");
+    return res.status(200).json({ message: "Logged out successfully" });
+  });
+});
 
 export default router;
